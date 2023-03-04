f19098384335804fb1b549db5c4de275f694985a
tavtejS07 <tavtej85@gmail.com>
2023-03-04 14:42:17 -0500

    Used page factory and @FindBy to streamline operator buttons

--- CalcTestFramework/src/calculatorTests/BasicOperations.java
+++ CalcTestFramework/src/calculatorTests/BasicOperations.java
@@ -2,15 +2,22 @@ package calculatorTests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.support.FindBy;
+//import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+//import calculatorTests.RepeatButtons;
 
 public class BasicOperations {
 	
 	WebDriver testDriver;
-	String calC = "http://output.jsbin.com/hudape/1/";
+	String calcLink = "http://output.jsbin.com/hudape/1/";
+	RepeatButtons rpt;
+	
 	
 	public static WebDriver newDriver() {
 		System.setProperty("webdriver.chrome.driver", "/Users/tavtejsingh/Documents/jarfiles_selenium/chromedriver");
@@ -20,18 +27,57 @@ public class BasicOperations {
 	@BeforeMethod
 	public void openUrl() {
 		testDriver = newDriver();
-		testDriver.get(calC);
+		testDriver.get(calcLink);
+		rpt = new RepeatButtons(testDriver);
 	}
 	
 	@Test
 	public void additionTest() {
 		testDriver.findElement(By.xpath("/html/body/div/div[3]/button[1]")).click();
-		testDriver.findElement(By.xpath("/html/body/div/div[3]/button[13]")).click();
+		rpt.clickAdd();
+		//testDriver.findElement(By.xpath("/html/body/div/div[3]/button[13]")).click();
 		testDriver.findElement(By.xpath("/html/body/div/div[3]/button[8]")).click();
-		testDriver.findElement(By.xpath("/html/body/div/div[3]/button[12]")).click();
+		//testDriver.findElement(By.xpath("/html/body/div/div[3]/button[12]")).click();
+		rpt.clickEqual();
+		//clickEqual(testDriver);
 		
 		String value = testDriver.findElement(By.id("output")).getText();
 		Assert.assertEquals("9", value, "Value mismatch test failed");
+	}
+	
+	@Test
+	public void subtractionTest() {
+		testDriver.findElement(By.xpath("/html/body/div/div[3]/button[6]")).click();
+		//testDriver.findElement(By.xpath("/html/body/div/div[3]/button[14]")).click();
+		rpt.clickSub();
+		testDriver.findElement(By.xpath("/html/body/div/div[3]/button[2]")).click();
+		//testDriver.findElement(By.xpath("/html/body/div/div[3]/button[12]")).click();
+		rpt.clickEqual();
+		String value = testDriver.findElement(By.id("output")).getText();
+		Assert.assertEquals("4", value, "Value mismatch test failed");
+	}
+	
+	@Test
+	public void divisionTest() {
+		testDriver.findElement(By.xpath("/html/body/div/div[3]/button[9]")).click();
+		//testDriver.findElement(By.xpath("/html/body/div/div[3]/button[10]")).click();
+		//testDriver.findElement(By.xpath("/html/body/div/div[3]/button[10]")).click();
+		//testDriver.findElement(By.xpath("/html/body/div/div[3]/button[10]")).click();
+		rpt.clickDiv();
+		//testDriver.findElement(By.xpath("/html/body/div/div[3]/button[16]")).click();
+		//testDriver.findElement(By.xpath("/html/body/div/div[3]/button[13]")).click();
+		testDriver.findElement(By.xpath("/html/body/div/div[3]/button[3]")).click();
+		rpt.clickEqual();
+		//testDriver.findElement(By.xpath("/html/body/div/div[3]/button[12]")).click();
+		
+		String value = testDriver.findElement(By.id("output")).getText();
+		//System.out.println(value);
+		Assert.assertEquals("3", value, "Value mismatch test failed");
+	}
+	
+	@AfterMethod
+	public void quitBrowser() {
+		testDriver.quit();
 	}
 	
 	
---
+++ CalcTestFramework/src/calculatorTests/RepeatButtons.java
@@ -0,0 +1,59 @@
+package calculatorTests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class RepeatButtons {
+	
+	WebDriver testDriver;
+	
+	@FindBy(xpath = "/html/body/div/div[3]/button[12]")
+	WebElement equalBtn;
+	
+	@FindBy(xpath = "/html/body/div/div[3]/button[13]")
+	WebElement addBtn;
+	
+	@FindBy(xpath = "/html/body/div/div[3]/button[14]")
+	WebElement subBtn;
+	
+	@FindBy(xpath = "/html/body/div/div[3]/button[15]")
+	WebElement mulBtn;
+	
+	@FindBy(xpath = "/html/body/div/div[3]/button[16]")
+	WebElement divBtn;
+	
+	@FindBy(xpath = "/html/body/div/div[3]/button[11]")
+	WebElement delBtn;
+	
+	public RepeatButtons(WebDriver testDriver) {
+		this.testDriver = testDriver;
+		PageFactory.initElements(testDriver, this);
+	}
+	
+	public void clickEqual() {
+		equalBtn.click();
+	}
+	
+	public void clickAdd() {
+		addBtn.click();
+	}
+	
+	public void clickSub() {
+		subBtn.click();
+	}
+	
+	public void clickMul() {
+		mulBtn.click();
+	}
+	
+	public void clickDiv() {
+		divBtn.click();
+	}
+	
+	public void clickDel() {
+		delBtn.click();
+	}
+
+}
